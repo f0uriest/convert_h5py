17d3c1a4443c76550d9d52173da6ca8df6caee25
Rory Conlin <roryconlin@gmail.com>
2021-07-05 23:15:19 -0400

    Add main function

--- convert_h5.py
+++ convert_h5.py
@@ -1,6 +1,7 @@
 import h5py
 import shutil
 import numpy as np
+import sys
 
 def convert_attrs(f):
     for key in f:
@@ -18,10 +19,16 @@ def convert_groups(f):
             convert_groups(f[key])
             
 def convert_h5py(file_in, file_out=None):
-    if file_out is not None:
+    if file_out:
         shutil.copy(file_in, file_out)
     else:
         file_out = file_in
     f = h5py.File(file_out, 'a')
     convert_groups(f)
     f.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) in [2, 3]:
+        convert_h5py(sys.argv[1], sys.argv[2:])
+    else:
+        print("Usage: convert_h5py file_in [file_out]")
